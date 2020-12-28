23565c583fc6b7b9993080d17a392ecb22a52b90
olivier.lediouris <olivier.lediouris@gmail.com>
2020-12-28 17:13:05 +0100

    Added Java version (no notebook)

--- Chapter 01/src/main/java/chap01/Section02.java
+++ Chapter 01/src/main/java/chap01/Section02.java
@@ -5,18 +5,20 @@ import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
 import org.knowm.xchart.XYSeries;
 import org.knowm.xchart.style.Styler;
+import org.knowm.xchart.style.colors.XChartSeriesColors;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
+import java.awt.BasicStroke;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Section02 {
 
     private static double[] mileages = new double[]{4.1429, 8.9173, 6.5, 6.0601, 12.3, 6.2, 2.5782, 0.9, 1.7, 13.1045, 24.7, 9.2699, 17.2, 10.0, 10.0, 2.8, 12.3773, 19.6, 7.3397, 2.1178, 12.9886, 10.9884, 16.9, 6.0, 12.9, 8.1936, 10.5, 8.0713, 1.7, 10.0, 15.6097, 17.0, 16.7, 5.6, 11.3, 19.9, 9.6, 21.6, 20.3};
-    private static double[] prices = new double[]{16980.0, 15973.0, 9900.0, 15998.0, 3900.0, 12540.0, 21688.0, 17086.0, 23000.0, 8900.0, 3875.0, 10500.0, 3500.0, 26992.0, 17249.0, 19627.0, 9450.0, 3000.0, 14999.0, 24990.0, 7967.0, 7257.0, 4799.0, 13982.0, 5299.0, 14310.0, 7800.0, 12250.0, 23000.0, 14686.0, 7495.0, 4950.0, 3500.0, 11999.0, 9600.0, 1999.0, 4300.0, 3500.0, 4200.0};
+    private static double[] prices = new double[]{16_980.0, 15_973.0, 9_900.0, 15_998.0, 3_900.0, 12_540.0, 21_688.0, 17_086.0, 23_000.0, 8_900.0, 3_875.0, 10_500.0, 3_500.0, 26_992.0, 17_249.0, 19_627.0, 9_450.0, 3_000.0, 14_999.0, 24_990.0, 7_967.0, 7_257.0, 4_799.0, 13_982.0, 5_299.0, 14_310.0, 7_800.0, 12_250.0, 23_000.0, 14_686.0, 7_495.0, 4_950.0, 3_500.0, 11_999.0, 9_600.0, 1_999.0, 4_300.0, 3_500.0, 4_200.0};
 
     private static double price(double mileage) {
-        return 26500 * Math.pow(0.905, mileage);
+        return 26_500 * Math.pow(0.905, mileage);
     }
 
     public static void main(String... args) {
@@ -75,12 +77,17 @@ public class Section02 {
                 new double[] {xTargetFrom, xTargetTo},
                 new double[] {targetPrice, targetPrice});
         target1.setMarker(SeriesMarkers.NONE);
-        target1.setXYSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line); // TODO Dotted line?
+        target1.setLineColor(XChartSeriesColors.RED);
+        target1.setXYSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
+        BasicStroke dottedStroke = new BasicStroke(1f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 1f, new float[]{5f, 5f}, 0f);
+        target1.setLineStyle(dottedStroke);
         XYSeries target2 = chart.addSeries("Target-X",
                 new double[] {targetMileage, targetMileage},
                 new double[] {0, targetPrice});
         target2.setMarker(SeriesMarkers.NONE);
-        target2.setXYSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line); // TODO Dotted line?
+        target2.setLineColor(XChartSeriesColors.RED);
+        target2.setXYSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
+        target2.setLineStyle(dottedStroke);
 
         new SwingWrapper(chart).displayChart();
     }
